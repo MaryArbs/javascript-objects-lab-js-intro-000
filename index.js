1c50ec73e4bde045712ddf638c139ce8b9e51cff
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-09-17 23:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value) {
-return Object.assign({}, object, {[key]: value});
+return Object.assign({}, object, {[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
+ obj [key] = value;
+ return recipes
+ }
+  
